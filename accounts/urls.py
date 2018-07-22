dba564b6bd575375a0014ae4d94cbdd75c10eb44
Elvin Ega <proega3@gmail.com>
2018-07-22 12:10:55 +0300

    :heart: Add view transactions by number

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^send_money/$', send_money),
     url(r'^view_accounts/$', view_accounts),
     url(r'^view_transactions/$', view_transactions),
-    url(r'^view_account_by_user_id/(?P<user_id>[0-9a-z-]+)/$', view_account_by_user_id)
+    url(r'^view_account_by_user_id/(?P<user_id>[0-9a-z-]+)/$', view_account_by_user_id),
+    url(r'^view_transactions_by_number/(?P<number>[0-9a-z-]+)/$', view_transactions_by_number)
 ]
